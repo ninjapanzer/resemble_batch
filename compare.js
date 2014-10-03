0b918feadc38b7a4f3b890ba7e5c00cf6a23d1fa
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-10-03 17:04:47 -0400

    now compares image files between baseline and screenshots folders

--- compare.js
+++ compare.js
@@ -1,18 +1,86 @@
-var page = require('webpage').create();
+var results = {};
 var fs = require('fs');
+var toBeProcessed = 0;
+var processed = 0;
+
+var onConsoleMessage = function(msg, lineNum, sourceId) {
+  var result = JSON.parse(msg);
+  results[result.file] = results[result.file] || [];
+  results[result.file].push(result.data);
+  fs.write('results.pnz', JSON.stringify(results,null,2), 'w');
+  toBeProcessed--;
+  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+};
+
+var baseline = fs.list('baseline');
+var filelist = new Array;
+for(var o in baseline) {
+  filelist.push(baseline[o]);
+}
+
+var compare = function(file){
+  var page = require('webpage').create();
+  page.file = file;
+  page.viewportSize = { width: 1920, height: 1080 };
+  page.onConsoleMessage = onConsoleMessage;
+  page.open('http://localhost:8080/bigger.html', function(){
+    page.evaluate(function(file) {
+      window.file = file;
+    }, page.file);
+    window.setTimeout(function(){
+      page.close();
+      return;
+    },3000);
+  });
+};
+
+var processSome = function(count){
+  toBeProcessed += count;
+  for (var i = 0; i <= count; i++){
+    var currentFile = filelist.pop();
+    if(!currentFile){
+      toBeProcessed--;
+      continue;
+    }
+    if(!currentFile.match(/\.(jpg|jpeg|png|gif)$/)){
+      toBeProcessed--;
+      //console.log('Not image '+currentFile);
+      continue;
+    }
+    if(!fs.exists('screenshots/'+currentFile)){
+      toBeProcessed--;
+      //console.log('Compared failed to find matching file for '+currentFile);
+      continue;
+    }
+    console.log('testing '+currentFile);
+
+    compare(currentFile);
+  }
+};
+
+processSome(1);
+
+var exitInterval = setInterval(function(){
+  if(filelist.length <= 0){
+    phantom.exit();
+  }else{
+    if(toBeProcessed == processed || toBeProcessed <= 0){
+      toBeProcessed = 0;
+      processed = 0;
+      processSome(1);
+    }
+    //console.log(filelist.length, ' to be ', toBeProcessed, ' processed ', processed);
+  }
+}, 500);
+
 //console.log('The default user agent is ' + page.settings.userAgent);
 //page.settings.userAgent = 'SpecialAgent';
 //page.content = '<html><body><p>Hello world</p></body></html>';
-page.viewportSize = { width: 1920, height: 1080 };
 
 //var one = fs.readFileSync('/Users/paulscarrone/workrepos/resemble_batch/images/onyx.jpg');
 //var two;
 
 //page.one = one;
-
-page.onConsoleMessage = function(msg, lineNum, sourceId) {
-  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
-};
 
 /*page.onLoadFinished = function (status) {
   if (status !== 'success') {
@@ -51,11 +119,3 @@ page.onConsoleMessage = function(msg, lineNum, sourceId) {
   }
 
 };*/
-
-page.open('http://localhost:8080/bigger.html', function(){
-  window.setTimeout(function(){
-    console.log(page.content);
-    page.render('my_home.jpeg', {format: 'jpeg', quality: '100'});
-    phantom.exit();
-  },5000);
-});

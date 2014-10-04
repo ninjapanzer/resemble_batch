a4d53dd32d6ebf9644446ec992e99416831e80cc
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-10-04 14:30:54 -0400

    makes test finish just in time

--- compare.js
+++ compare.js
@@ -2,6 +2,8 @@ var results = {};
 var fs = require('fs');
 var toBeProcessed = 0;
 var processed = 0;
+var total = 0;
+var batch = 2;
 
 var onConsoleMessage = function(msg, lineNum, sourceId) {
   var result = JSON.parse(msg);
@@ -9,14 +11,16 @@ var onConsoleMessage = function(msg, lineNum, sourceId) {
   results[result.file].push(result.data);
   fs.write('results.pnz', JSON.stringify(results,null,2), 'w');
   toBeProcessed--;
-  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  processed++;
+  //console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 };
 
 var baseline = fs.list('baseline');
-var filelist = new Array;
+var filelist = [];
 for(var o in baseline) {
   filelist.push(baseline[o]);
 }
+total = filelist.length;
 
 var compare = function(file){
   var page = require('webpage').create();
@@ -27,28 +31,29 @@ var compare = function(file){
     page.evaluate(function(file) {
       window.file = file;
     }, page.file);
-    window.setTimeout(function(){
-      page.close();
-      return;
-    },3000);
+    // Wait for analysis to be done
+    window.setInterval(function(){
+      if(window.done){
+        page.close();
+        return;
+      }
+    }, 500);
   });
 };
 
 var processSome = function(count){
   toBeProcessed += count;
-  for (var i = 0; i <= count; i++){
+  for (var i = 0; i < count; i++){
     var currentFile = filelist.pop();
-    if(!currentFile){
-      toBeProcessed--;
-      continue;
-    }
     if(!currentFile.match(/\.(jpg|jpeg|png|gif)$/)){
       toBeProcessed--;
+      processed++;
       //console.log('Not image '+currentFile);
       continue;
     }
     if(!fs.exists('screenshots/'+currentFile)){
       toBeProcessed--;
+      processed++;
       //console.log('Compared failed to find matching file for '+currentFile);
       continue;
     }
@@ -58,64 +63,15 @@ var processSome = function(count){
   }
 };
 
-processSome(1);
+processSome(batch);
 
 var exitInterval = setInterval(function(){
   if(filelist.length <= 0){
     phantom.exit();
   }else{
-    if(toBeProcessed == processed || toBeProcessed <= 0){
-      toBeProcessed = 0;
-      processed = 0;
-      processSome(1);
+    if(toBeProcessed == 0){
+      processSome(batch);
     }
-    //console.log(filelist.length, ' to be ', toBeProcessed, ' processed ', processed);
+    console.log(filelist.length, ' to be ', toBeProcessed, ' processed ', processed, ' of ', total);
   }
 }, 500);
-
-//console.log('The default user agent is ' + page.settings.userAgent);
-//page.settings.userAgent = 'SpecialAgent';
-//page.content = '<html><body><p>Hello world</p></body></html>';
-
-//var one = fs.readFileSync('/Users/paulscarrone/workrepos/resemble_batch/images/onyx.jpg');
-//var two;
-
-//page.one = one;
-
-/*page.onLoadFinished = function (status) {
-  if (status !== 'success') {
-    console.log('Unable to access network');
-  } else {
-    page.injectJs('bower_components/jquery/dist/jquery.js');
-    page.injectJs('node_modules/resemblejs/resemble.js');
-    var respData;
-    var callback = function(data){
-      console.log(JSON.stringify(data,null,2));
-    };
-    var ua = page.evaluate(function(callback) {
-      console.log('hi');
-      var imgOne = new Image();
-      imgOne.src = 'http://localhost:8080/images/onyx.jpg';
-      imgOne.height = '500';
-      imgOne.width = '500';
-      var imgTwo = new Image();
-      imgTwo.height = '500';
-      imgTwo.width = '500';
-      imgTwo.src = 'http://localhost:8080/images/roger.png';
-      imgOne.id = "unhappy";
-      imgTwo.id = "happy";
-      $('body').append(imgOne);
-      $('body').append(imgTwo);
-      window.setTimeout(function(){
-        console.log(JSON.stringify(resemble(page.one)), null,2);
-      },2000);
-    });
-    //console.log(JSON.stringify(ua,null,2));
-    window.setTimeout(function(){
-      page.render('my_home.jpeg', {format: 'jpeg', quality: '100'});
-      //console.log(page.content);
-      phantom.exit();
-    }, 3000);
-  }
-
-};*/
